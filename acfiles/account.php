4e6a26915176a336f280908e15db33b3587b5835
sebinpj <jishnutoop@gmail.com>
2016-03-14 20:44:36 +0530

    finished linking db to doctor and patient

--- acfiles/account.php
+++ acfiles/account.php
@@ -1,9 +1,54 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Sebin PJ
- * Date: 2/15/2016
- * Time: 2:58 PM
- */
-print_r($_POST);
+require("dbsettings.php");
+$user = $_COOKIE["id"];
+if($user != "") {
+    $chkacqry = "SELECT * FROM `odcs`.`allusers` WHERE uid='$user'";
+    mysqli_select_db($dbhandle, $mysqlidb);
+    $result = mysqli_query($dbhandle, $chkacqry) or die("<h2> Somethings Up </h2> <br> <div align=\"center\" style =\"margin:0 auto\" class=\"neutral\"><span></span></div> <br> <br>" . mysqli_error($dbhandle));
+    $count = mysqli_num_rows($result);
+    $row = mysqli_fetch_assoc($result);
+    $name = $row['fname'];
+    $newURL  = "http://localhost/ODCS/";
+    $usern = $row["username"];
+    $atype = $row["actp"];
+    if ($count == 1){
+        $flag =1;
+    }else{
+        $flag =0;
+    }
+    if($atype == 'Patient') {
+        echo "<br> Patient <br>";
+        $address = $_POST['adr'];
+        $gender = $_POST['g'];
+        $uid = $user;
+        $height = $_POST['h'];
+        $weight = $_POST['w'];
+        $dob = $_POST['d'];
+        $insertq = "INSERT INTO `odcs`.`patient` (`address`, `gender`, `height`, `weight`, `dob`, `uid`) VALUES ('$address', '$gender', '$height', '$weight', '$dob', '$uid')";
+        $result = mysqli_query($dbhandle, $insertq) or die("<h2> Somethings Up </h2> <br> <div align=\"center\" style =\"margin:0 auto\" class=\"neutral\"><span></span></div> <br> <br>" . mysqli_error($dbhandle));
+        setcookie("id", $uid, time() + (86400 * 30), "/");
+        header('Location: '.$newURL);
+    }else if($atype == 'Doctor') {
+        //echo "<br> Doctor <br>";
+        $address = $_POST['adr'];
+        $gender = $_POST['g'];
+        $experience = $_POST['ex'];
+        $contact = $_POST['cn'];
+        $hospital = $_POST['hn'];
+        $qualification = $_POST['qn'];
+        $special = $_POST['sp'];
+        $uid = $user;
+        $speciality = explode(",",$special);
+        $max = sizeof($speciality);
+        for($i = 0; $i < $max;$i++)
+        {
+          $insertq = "INSERT INTO `odcs`.`doctor` (`uid`, `address`, `gender`, `experiance`, `contact`, `hospital`, `Qualification`, `speciality`) VALUES ('$uid', '$address', '$gender', '$experience', '$contact', '$hospital', '$qualification', '".$speciality[$i]."')";
+            $result = mysqli_query($dbhandle, $insertq) or die("<h2> Somethings Up </h2> <br> <div align=\"center\" style =\"margin:0 auto\" class=\"neutral\"><span></span></div> <br> <br>" . mysqli_error($dbhandle));
+           echo $speciality[$i];
+        }
+
+        setcookie("id", $uid, time() + (86400 * 30), "/");
+        header('Location: '.$newURL);
+    }
+}
 ?>
