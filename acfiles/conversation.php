404ec10536a28401f950f7936897b78e2a028c1b
sebinpj <jishnutoop@gmail.com>
2016-03-14 22:42:13 +0530

    added conversation page
    lallalalala

---
+++ acfiles/conversation.php
@@ -0,0 +1,137 @@
+
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery-1.12.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<div class="container">
+    <div class="page-header">
+        <h1 class="text-center">Who am I?</h1>
+    </div>
+    <p class="lead text-center">I am a creative graphic designer focused on modern eye catching designs. I am not only the graphic designer! I can also do complete websites &amp; computer applications.</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-info">
+                    <div class="panel-heading">
+                        Comments
+                    </div>
+                    <div class="panel-body comments">
+                        <textarea class="form-control" placeholder="Write your comment" rows="5"></textarea>
+                        <br>
+                        <a class="small pull-left" href="#">Login to you existing account</a>
+                        <button type="button" class="btn btn-info pull-right">Submit comment</button>
+                        <div class="clearfix"></div>
+                        <hr>
+                        <ul class="media-list">
+                            <li class="media">
+                                <div class="comment">
+                                    <a href="#" class="pull-left">
+                                        <img src="http://lorempixel.com/60/60/animals/?sf5saf" alt="" class="img-circle">
+                                    </a>
+                                    <div class="media-body">
+                                        <strong class="text-success">Jane Doe</strong>
+                  <span class="text-muted">
+                  <small class="text-muted">6 days ago</small>
+                  </span>
+                                        <p>
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                            Lorem ipsum dolor sit amet, <a href="#">#some link </a>.
+                                        </p>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <ul class="media-list">
+                                    <li class="media">
+                                        <div class="comment">
+                                            <a href="#" class="pull-left">
+                                                <img src="http://lorempixel.com/60/60/animals/?Fsf6sf6" alt="" class="img-circle">
+                                            </a>
+                                            <div class="media-body">
+                                                <strong class="text-success">MrAwesome</strong>
+                      <span class="text-muted">
+                      <small class="text-muted">2 days ago</small>
+                      </span>
+                                                <p>
+                                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                                    Lorem ipsum dolor sit amet.
+                                                </p>
+                                            </div>
+                                            <div class="clearfix"></div>
+                                        </div>
+                                    </li>
+                                    <li class="media">
+                                        <div class="comment">
+                                            <a href="#" class="pull-left">
+                                                <img src="http://lorempixel.com/60/60/animals/?Fsgdsg" alt="" class="img-circle">
+                                            </a>
+                                            <div class="media-body">
+                                                <strong class="text-success">Miss Lucia</strong>
+                      <span class="text-muted">
+                      <small class="text-muted">15 minutes ago</small>
+                      </span>
+                                                <p>
+                                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                                    Lorem ipsum dolor sit amet.
+                                                </p>
+                                            </div>
+                                            <div class="clearfix"></div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="media">
+                                <div class="comment">
+                                    <a href="#" class="pull-left">
+                                        <img src="http://lorempixel.com/60/60/animals/?qrwegsg" alt="" class="img-circle">
+                                    </a>
+                                    <div class="media-body">
+                                        <strong class="text-success">Jana Cova</strong>
+                  <span class="text-muted">
+                  <small class="text-muted">12 days ago</small>
+                  </span>
+                  <span class="text-muted pull-right">
+                  <small class="btn btn-danger btn-xs"><i class="fa fa-times"></i> Remove</small>
+                  </span>
+                                        <p>
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                            Lorem ipsum dolor sit amet.
+                                        </p>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </li>
+                            <li class="media">
+                                <div class="comment">
+                                    <a href="#" class="pull-left">
+                                        <img src="http://lorempixel.com/60/60/animals/?setrtss" alt="" class="img-circle">
+                                    </a>
+                                    <div class="media-body">
+                                        <strong class="text-success">Johnatan Smarty</strong>
+                  <span class="text-muted">
+                  <small class="text-muted">1 month ago</small>
+                  </span>
+                                        <p>
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+                                        </p>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sebin PJ
+ * Date: 3/14/2016
+ * Time: 10:38 PM
+ */
